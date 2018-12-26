f675865bc4ef9f994efc446ea97b113f1b7ab002
Stancu Florin <niflostancu@gmail.com>
2018-12-26 16:21:27 +0200

    add tmux & git to the install script

--- install.sh
+++ install.sh
@@ -3,7 +3,17 @@
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 
+# ZSH
 bash "$SRC_DIR/zsh/install.sh"
+[[ "$SHELL" != "/usr/bin/zsh" ]] && chsh /usr/bin/zsh
+
+# Tmux
+bash "$SRC_DIR/tmux/install.sh"
+
+# Git config
+if [[ ! -f "$XDG_CONFIG_HOME/git" ]]; then
+  ln -s "$SRC_DIR/git" "$XDG_CONFIG_HOME/git"
+fi
 
 # Install the profile scripts
 ln -s "$SRC_DIR/profile.sh" "$XDG_CONFIG_HOME/profile.sh"
