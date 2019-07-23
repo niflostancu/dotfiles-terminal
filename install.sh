3a9cfc3357bffefedd364c459d94f63f6708b188
Stancu Florin <niflostancu@gmail.com>
2019-07-23 14:43:42 +0300

    moved tmux & git config outside of this repo

--- install.sh
+++ install.sh
@@ -7,14 +7,6 @@ XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 bash "$SRC_DIR/zsh/install.sh"
 [[ "$SHELL" != "/usr/bin/zsh" ]] && chsh /usr/bin/zsh
 
-# Tmux
-bash "$SRC_DIR/tmux/install.sh"
-
-# Git config
-if [[ ! -f "$XDG_CONFIG_HOME/git" ]]; then
-  ln -s "$SRC_DIR/git" "$XDG_CONFIG_HOME/git"
-fi
-
 # Install the profile scripts
 ln -s "$SRC_DIR/profile.sh" "$XDG_CONFIG_HOME/profile.sh"
 
