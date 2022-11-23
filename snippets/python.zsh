950d2faae30945cfac3c701d9dd714dabd9b1536
Stancu Florin <niflostancu@gmail.com>
2022-11-23 14:02:17 +0200

    snippets: python: finally fix pyenv
    (use init hook to re-genereate / update pyenv's init file)

--- snippets/python.zsh
+++ snippets/python.zsh
@@ -3,13 +3,15 @@
 
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 
-zi ice atclone'{ 
-    export PYENV_ROOT="$PWD"; ./libexec/pyenv init -;
+zi ice \
+  submods='pyenv/pyenv-virtualenv -> plugins/pyenv-virtualenv' \
+  atinit'
+  export PYENV_ROOT="$PWD";
+  [ -f zpyenv.zsh ] || { 
+    ./libexec/pyenv init -;
     ./libexec/pyenv virtualenv-init -;
     echo "rehash"
-  } > zpyenv.zsh' \
-  submods='pyenv/pyenv-virtualenv -> plugins/pyenv-virtualenv' \
-  atinit'export PYENV_ROOT="$PWD"' atpull"%atclone" run-atpull \
+  } >zpyenv.zsh' \
   as'program' pick'bin/pyenv' src"zpyenv.zsh" nocompile'!'
 zi_load pyenv/pyenv
 
