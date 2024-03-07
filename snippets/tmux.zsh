5c2d43dcca94329b307f2c6687eaae910db59fe0
Stancu Florin <niflostancu@gmail.com>
2024-03-07 19:12:29 +0200

    tmux: autostart fastfetch (neofetch) on first pane

--- snippets/tmux.zsh
+++ snippets/tmux.zsh
@@ -9,3 +9,13 @@ if [[ -f "$TMUX_INTEGRATION_PLUGIN" ]]; then
   source "$TMUX_INTEGRATION_PLUGIN"
 fi
 
+if [[ -n "$TMUX_PANE" ]]; then
+  _TMUX_SESS_WIN_PANE="$(tmux display-message -p '#S/#I/#P')"
+  if [[ "$_TMUX_SESS_WIN_PANE" == "$TMUX_AUTOSTART_SESSION/1/1" ]]; then
+    if type "fastfetch" > /dev/null; then
+      echo
+      fastfetch
+    fi
+  fi
+fi
+
